66fdcf1be9a40baf80e9b342e64786ceeb13e9c9
MRT <michaelraythomas.hou@gmail.com>
2021-02-01 18:45:27 -0600

    read csv set up

---
+++ PyBank/Main.py
@@ -0,0 +1,9 @@
+# Import the os module
+# This will allow us to create file paths across operating systems
+import os
+
+# Module for reading CSV files
+import csv
+
+csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+
