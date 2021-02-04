64af68e65688a4a4c884a2a7ca382abeeae45de9
MRT <michaelraythomas.hou@gmail.com>
2021-02-04 17:37:29 -0600

    clean up

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -12,8 +12,6 @@ with open(csvpath) as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
-    
-    print(csvreader)
 
     # Read the header row first (skip this step if there is now header)
     csv_header = next(csvreader)
