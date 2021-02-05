ad35ae1c2aab1cca718fed2a29f441cc6d2b8ab0
MRT <michaelraythomas.hou@gmail.com>
2021-02-04 18:29:11 -0600

    clean boolean

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -20,8 +20,8 @@ with open(csvpath) as csvfile:
     Months = []
     # Make a list of profits...Jesus, Muhammed, Soloman, The Simpsons, etc.
     Profit = []
-    # Add a 0 to the list to provide starting variable for calc of diff as second to last
-    Profit.append(0)
+    # Add a boolean variable for calc of diff for second to last
+    First = True
     # Make a list of the Difference in profits from month to month...Christian, Muslim, Byzantine, Fox Tv, etc.
     Diff_Profit = []
     
@@ -32,17 +32,18 @@ with open(csvpath) as csvfile:
         # Add Integers to Profit list
         Profit.append(int(row[1]))
         # Add the Calculated Difference to Diff_Profit List
-        Diff_Profit.append(Profit[-1] - Profit[-2])
+        if not First == True:
+            Diff_Profit.append(Profit[-1] - Profit[-2])
+        else:
+            First = False
 
-    # Remove the first element 0 previously added for calculation purposes
-    Profit.pop(0)
     
     # Print my final analysis
     print(f'Financial Analysis')
     print(f'------------------')
     print(f'Total Months: {len(Months)}')
     print(f'Total Profit: {sum(Profit)}')
-    print(f'Average Change: {(sum(Diff_Profit) / len(Months))}')
+    print(f'Average Change: {(sum(Diff_Profit) / len(Diff_Profit)):.2f}')
     print(f'Greatest Increase in Profit: {Months[Diff_Profit.index(max(Diff_Profit))]} {max(Diff_Profit)}')
     print(f'Greatest Decrease in Profit: {Months[Diff_Profit.index(min(Diff_Profit))]} {min(Diff_Profit)}')
     print(f'------------------')
